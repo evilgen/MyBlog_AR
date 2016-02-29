28ff89993f8e6c44d353b4619bce814988961f1b
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-02-29 19:34:24 +0300

    Add stub for '/new'

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#Создание подключения к БД при помощи Active Record
+set :database, "sqlite3:MyNewBlog.db"
+
 
 get '/' do
-	erb "Hello!"			
+	erb :new			
+end
+
+get '/new' do
+  "Hello World"
 end
